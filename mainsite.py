a2c9ab5f4750bd2f42afcd678b1023e364553d5f
Sky Jones <rsajones94@gmail.com>
2019-06-16 20:53:25 -0500

    Add newman projection

--- mainsite.py
+++ mainsite.py
@@ -10,5 +10,25 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/pyfluv")
+def pyfluv():
+    return render_template('pyfluv.html', title='pyfluv')
+
+@app.route("/ecotrajectory")
+def ecotrajectory():
+    return render_template('ecotrajectory.html', title='ecotrajectory')
+
+@app.route("/molec-strain")
+def molec_strain():
+    return render_template('molec-strain.html', title='Newman Projection')
+
+@app.route("/geoengine")
+def geoengine():
+    return render_template('geoengine.html', title='KCI Geoengine')
+
+@app.route("/more-projects")
+def more_projects():
+    return render_template('more-projects.html', title='More Projects')
+
 if __name__ == '__main__':
     app.run(debug=True)
